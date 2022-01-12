73d299779727360cef44ee7d09efe0a9a2b57a96
20wh1a1254 <93141159+20wh1a1254@users.noreply.github.com>
2022-01-12 20:14:52 +0530

    Add files via upload

---
+++ C++ LAB/Experiment 7/scoperesolution.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#define PI 3.14
+class circle
+{
+    float radius;
+    public:
+    int area();
+};
+int circle::area()
+{
+    radius = 10;
+    return PI*radius*radius;
+}	
+int a = 50;
+int main()
+{
+    circle c;
+    int num = 100;
+    std::cout<<num<<std::endl;
+    std::cout<<::a<<std::endl;
+    std::cout<<"Area:"<<c.area()<<std::endl;
+    return 0;
+}
