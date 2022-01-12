7a59442ca44041a357c99926f3ceeebdcc9011ae
20wh1a1254 <93141159+20wh1a1254@users.noreply.github.com>
2022-01-12 18:45:42 +0530

    Update pointer.cpp

--- C++ LAB/Experiment 3/pointer.cpp
+++ C++ LAB/Experiment 3/pointer.cpp
@@ -1,1 +1,34 @@
-ffcfrthhj
+#include <iostream>
+#include <string>
+using namespace std;
+class Employee
+{
+    string name;
+    int id;
+    float salary;
+    public:
+    void read()
+    {
+        cout<<"Enter the name of employee: ";
+	cin>>name;
+	cout<<"Enter the ID of employee: ";
+	cin>>id;
+	cout<<"Enter the Salary: ";
+	cin>>salary;
+    }
+    void display()
+    {
+        cout<<"Name of the employee: "<<name<<endl;
+	cout<<"ID of the employee: "<<id<<endl;
+	cout<<"Salary of the employee: "<<salary<<endl;
+    }
+};
+int main()
+{
+    Employee e;
+    Employee *ptr;
+    ptr = &e;
+    ptr->read();
+    ptr->display();
+    return 0;
+}
