cf8eb98fa8265cfa9cc5a8007dd1cc538e9f2d3d
20wh1a1254 <93141159+20wh1a1254@users.noreply.github.com>
2022-01-13 10:45:18 +0530

    Add files via upload

---
+++ C++ LAB/Experiment 10/thispointer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Employee
+{
+    public:
+    int id;
+    string name;
+    float salary;
+    Employee(int id, string name, float salary)
+    {
+        this->id = id;
+	this->name = name;
+	this->salary = salary;
+    }
+    void display()
+    {
+	cout<<id<<" "<<name<<" "<<salary<<endl;
+    }
+};
+int main(void)
+{
+    Employee e1 =Employee(1, "Neelima", 100000);
+    Employee e2 =Employee(2, "Vyshali", 50000);
+    e1.display();
+    e2.display();
+    return 0;
+}
